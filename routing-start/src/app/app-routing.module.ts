3cc25f9d380774b3f2bc4f755393e19842703183
John Garner <jdextergarneriv@gmail.com>
2021-03-09 16:54:23 -0500

    export routermodule from app-routes and import AppRoutingModule in app.module

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { Routes } from "@angular/router";
+import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
@@ -27,5 +27,8 @@ const appRoutes: Routes = [
   { path: "**", redirectTo: "/not-found" },
 ];
 
-@NgModule({})
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
 export class AppRoutingModule {}
