5325b584505be530f08d18e66e5d85f9024f1fbc
John Garner <jdextergarneriv@gmail.com>
2021-03-10 12:03:14 -0500

    create server-resolver, inject ServersService and return .getServer(+route.params['id'])

--- routing-start/src/app/servers/server/server.component.ts
+++ routing-start/src/app/servers/server/server.component.ts
@@ -18,11 +18,11 @@ export class ServerComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const id = parseInt(this.route.snapshot.params["id"]);
-    this.server = this.serversService.getServer(id);
-    this.route.params.subscribe((params: Params) => {
-      this.server = this.serversService.getServer(parseInt(params["id"]));
-    });
+    // const id = parseInt(this.route.snapshot.params["id"]);
+    // this.server = this.serversService.getServer(id);
+    // this.route.params.subscribe((params: Params) => {
+    //   this.server = this.serversService.getServer(parseInt(params["id"]));
+    // });
   }
 
   onEdit() {
